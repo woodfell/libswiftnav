462d1389f80bc58109faaff75b54356610114e56
mbavaro <michele.bavaro@gmail.com>
2018-12-13 10:58:32 -0800

    fix comment

--- src/single_epoch_solver.c
+++ src/single_epoch_solver.c
@@ -1193,7 +1193,7 @@ s8 calc_PVT(const u8 n_used,
                    (sats_used <= N_STATE + RAIM_MAX_EXCLUSIONS);
         break;
       case L1_ONLY:
-        /* use all the available 1575.4 2MHz codes */
+        /* use all the available 1575.42 MHz codes */
         use_this = (CODE_GPS_L1CA == nav_meas[i].sid.code) ||
                    (CODE_GAL_E1B == nav_meas[i].sid.code);
         break;
